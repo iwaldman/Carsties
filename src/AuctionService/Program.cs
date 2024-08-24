86e285351c9b0d27a8ba1e3b35074e52840015bb
iwaldman <irvin.waldman@hotmail.com>
2024-08-23 20:35:15 -0500

    Shaping the data to return

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<AuctionDbContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
