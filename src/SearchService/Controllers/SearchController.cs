222bdf90241fb5358350de50ca359a373b70f821
iwaldman <irvin.waldman@hotmail.com>
2024-08-27 18:33:37 -0500

    feat: Add filtering and ordering

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers;
 
@@ -11,22 +12,50 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(
-        string? searchTerm,
-        int pageNumber = 1,
-        int pageSize = 10
-    )
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParams searchParams)
     {
-        var query = DB.PagedSearch<Item>();
+        var query = DB.PagedSearch<Item, Item>();
 
-        query.Sort(x => x.Ascending(a => a.Make));
-
-        if (!string.IsNullOrWhiteSpace(searchTerm))
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
 
-        query.PageNumber(pageNumber).PageSize(pageSize);
+        query = searchParams.OrderBy switch
+        {
+            "make"
+                => query.Sort(x => x.Ascending(x => x.Make)).Sort(x => x.Ascending(a => a.Model)),
+            "new" => query.Sort(x => x.Descending(x => x.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(x => x.AuctionEnd))
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.FilterBy))
+        {
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon"
+                    => query.Match(
+                        x =>
+                            x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                            && x.AuctionEnd > DateTime.UtcNow
+                    ),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow) // live
+            };
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
 
