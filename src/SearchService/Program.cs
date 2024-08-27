763165163279da8a8e85b9ad5ff1bdf7ebc90c06
iwaldman <irvin.waldman@hotmail.com>
2024-08-26 20:20:31 -0500

    Adding the Item model

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,6 +9,19 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 app.MapControllers();
+
+await DB.InitAsync(
+    "SearchDb",
+    MongoClientSettings.FromConnectionString(
+        builder.Configuration.GetConnectionString("MongoDbConnection")
+    )
+);
+
+await DB.Index<Item>()
+    .Key(s => s.Make, KeyType.Text)
+    .Key(s => s.Model, KeyType.Text)
+    .Key(s => s.Color, KeyType.Text)
+    .CreateAsync();
 
 app.UseAuthorization();
 
