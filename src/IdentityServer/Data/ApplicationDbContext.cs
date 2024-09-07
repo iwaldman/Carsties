a9ca349c83301dba88c64cedb0613f506852c1b4
iwaldman <irvin.waldman@hotmail.com>
2024-09-07 11:04:12 -0500

    Adding IdentityServer

---
+++ src/IdentityServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using IdentityServer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace IdentityServer.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString =
+            "Server=localhost:5432; User Id=postgres; Password=postgrespw; Database=identity";
+        optionsBuilder.UseNpgsql(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
